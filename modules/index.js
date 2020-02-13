83e65987cfe725482269fc042632727d1c919bc3
Simon <simonsch.tz@gmail.com>
2020-02-13 14:03:02 +0100

    Make all percentages and difference work

--- modules/index.js
+++ modules/index.js
@@ -20,6 +20,27 @@ moduloForm.addEventListener("change", () => {
     document.querySelector("#modulo_result").value = modulo(modulo1, modulo2)
 })
 
+const percentageForm = document.querySelector("#percentage-form")
+percentageForm.addEventListener("change", () => {
+    const num1 = document.querySelector("#percentage_1").value
+    const num2 = document.querySelector("#percentage_2").value
+    document.querySelector("#percentage_result").value = percentage(num1, num2)
+})
+
+const percentageOfForm = document.querySelector("#percentageOf-form")
+percentageOfForm.addEventListener("change", () => {
+    const num1 = document.querySelector("#percentageOf_1").value
+    const num2 = document.querySelector("#percentageOf_2").value
+    document.querySelector("#percentageOf_result").value = percentage(num1, num2)
+})
+
+const differenceForm = document.querySelector("#difference-form")
+differenceForm.addEventListener("change", () => {
+    const num1 = document.querySelector("#difference_1").value
+    const num2 = document.querySelector("#difference_2").value
+    document.querySelector("#difference_result").value = percentage(num1, num2)
+})
+
 
 const aspectForm = document.querySelector("#aspect-form")
 aspectForm.addEventListener("change", () => {
