9824471f5b4e71e716cc73d4fa704ba66b301cf7
Simon <simonsch.tz@gmail.com>
2020-02-13 10:27:11 +0100

    export all .js and import them in index.js + link index.js in index.html

--- modules/index.js
+++ modules/index.js
@@ -1,1 +1,16 @@
 // Your code goes here!
+
+import { calculateAspectRatio } from "./aspect-ratio";
+import { modulo } from "./percentage"
+import { percentage } from "./percentage"
+import { percentageOf } from "./percentage"
+import { difference } from "./percentage"
+
+// import * as percent from "./percentage"
+// imports everything, to use you need something like percent.modulo.*...* 
+
+calculateAspectRatio();
+modulo();
+percentage();
+percentageOf();
+difference();
